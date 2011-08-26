97989ffc2d2951ac4bf6bd016e814810dbd1a48b
purvaDiaspark <purva.zore@gmail.com>
2011-08-26 14:45:33 +0530

    file temp

---
+++ testLog/temp.h
@@ -0,0 +1,16 @@
+//
+//  temp.h
+//  testLog
+//
+//  Created by MAC21 on 26/08/11.
+//  Copyright 2011 diaspark India. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface temp : UIViewController {
+    
+}
+
+@end
